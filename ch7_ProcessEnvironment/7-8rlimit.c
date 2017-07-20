9ac4b551d763eebcf3d23cbcec0b1987b1c07fa8
fflyelephant <2511988284@qq.com>
2017-07-20 22:12:51 +0800

    add some file

--- ch7_ProcessEnvironment/7-8rlimit.c
+++ ch7_ProcessEnvironment/7-8rlimit.c
@@ -91,18 +91,18 @@ int main(int argc, char const *argv[])
 /*result:
 
 	stone@cdWSCMPL07:~/test_my/github_test/APUE/ch7_ProcessEnvironment$ ./a.out 
-	RLIMIT_AS      (infinity) (infinity) 
-	RLIMIT_CORE             0 (infinity) 
-	RLIMIT_CPU     (infinity) (infinity) 
-	RLIMIT_DATA    (infinity) (infinity) 
-	RLIMIT_FSIZE   (infinity) (infinity) 
-	RLIMIT_NOFILE        1024      65536 
-	RLIMIT_STACK      8388608 (infinity) 
-	RLIMIT_MEMLOCK      65536      65536 
-	RLIMIT_MSGQUEUE    819200     819200 
-	RLIMIT_NICE             0          0 
-	RLIMIT_NPROC        32016      32016 
-	RLIMIT_RSS      (infinity) (infinity) 
-	RLIMIT_SIGPENDING    32016      32016 
+	RLIMIT_AS      (infinity) (infinity) //进程可获得的最大的存储区长度
+	RLIMIT_CORE             0 (infinity) //core文件的最大字节数，0表示不产生core dump文件
+	RLIMIT_CPU     (infinity) (infinity) //一个CPU时间片的最大量值(秒)，如果超过软限制进程将收到内核发出的SIGXCPU信号
+	RLIMIT_DATA    (infinity) (infinity) //数据段的最大字节数(此数据段 = data段 + bss段 + 堆长度)
+	RLIMIT_FSIZE   (infinity) (infinity) //可创建的文件的最大字节数，超过限制进程会收到内核发出的SIGXFSZ
+	RLIMIT_NOFILE        1024      65536 //一个进程可以打开的最多的文件描述符个数
+	RLIMIT_STACK      8388608 (infinity) //栈的最大字节数
+	RLIMIT_MEMLOCK      65536      65536 //进程中使用mlock能够锁住的最大存储区字节数
+	RLIMIT_MSGQUEUE    819200     819200 //进程能够分配给信号队列的最大存储字节数
+	RLIMIT_NICE             0          0 //进程的nice值，越高表示越容易被内核优先调度执行
+	RLIMIT_NPROC        32016      32016 //每个子进程中最大数量的real user id个数
+	RLIMIT_RSS      (infinity) (infinity) //?
+	RLIMIT_SIGPENDING    32016      32016 //一个进程中信号排队的最大个数
 
 */
