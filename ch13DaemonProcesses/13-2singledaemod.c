1a9ab0a3a7bbbd76cbc63b2a365b12a3d8dfedab
fflyelephant <2511988284@qq.com>
2017-08-29 23:05:55 +0800

    add chapter13

---
+++ ch13DaemonProcesses/13-2singledaemod.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <syslog.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <pthread.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define LOCKFILE "/var/run/daemon.pid"
+#define LOCKMODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
+
+extern int lockfile(int fd);
+
+int already_running(void)
+{
+	int 	fd;
+	char	buf[16];
+
+	fd = open(LOCKFILE, O_RDWR|O_CREAT, LOCKMODE);
+	if(fd < 0){
+		syslog(LOG_ERR, "can't open %s:%s", LOCKFILE, strerror(errno));
+		exit(1);
+	}
+
+	if(lockfile(fd) < 0){
+		if(errno == EACCES || errno == EAGAIN){
+			close(fd);
+			return 1;
+		}
+		syslog(LOG_ERR, "can't lock %s:%s", LOCKFILE, strerror(errno));
+		exit(1);
+	}
+
+	ftruncate(fd, 0);
+	sprintf(buf, "%ld", (long)getpid());
+	write(fd, buf, strlen(buf)+1);
+	return 0;
+}
+
+/*
+1:仅允许daemon的一个副本操作LOCKFILE文件写入pid,其它的daemon副本会在lockfile时返回EACCES或者EAGAIN
+
+*/
